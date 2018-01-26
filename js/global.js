21567f372729df7c8a744b552eb1f64fab203442
Sasha15 <alex_brovko@mail.ru>
2018-01-26 11:22:11 +0200

    Added filter, search and load more

--- js/global.js
+++ js/global.js
@@ -138,6 +138,129 @@
 		});
 
 
+
+		// quick search regex
+		var qsRegex;
+		var buttonFilter;
+
+		// use value of search field to filter
+		var $quicksearch = $('.quicksearch').keyup( debounce( function() {
+			qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+			$container.isotope();
+		}) );
+
+		// debounce so filtering doesn't happen every millisecond
+		function debounce( fn, threshold ) {
+			var timeout;
+			return function debounced() {
+				if ( timeout ) {
+					clearTimeout( timeout );
+				}
+				function delayed() {
+					fn();
+					timeout = null;
+				}
+				setTimeout( delayed, threshold || 100 );
+			};
+		}
+		// ISOTOPE PLUGIN SCRIPT//
+
+		// init Isotope
+		var $container = $('.isotope').isotope({
+			itemSelector: '.element-item',
+			layoutMode: 'masonry',
+			// masonry: {
+			// 	columnWidth: 100,
+			// 	  // fitWidth: true
+			// },
+			filter: function() {
+				var $this = $(this);
+				var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
+				var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
+				return searchResult && buttonResult;
+			},
+			getSortData: {
+				date: '.date',
+				popularity: '.popularity parseInt',
+			}
+		});
+
+		// bind filter button click
+		$('.filter-button').on('click', function() {
+			buttonFilter = $(this).attr('data-filter');
+			// use filterFn if matches value
+			// filterValue = filterFns[filterValue] || filterValue;
+			$container.isotope();
+			return false;
+		});
+
+		// bind sort button click
+		$('.sort-button').on('click', function() {
+			var sortByValue = $(this).attr('data-sort-by');
+			$container.isotope({
+				sortBy: sortByValue
+			});
+			return false;
+		});
+
+		// change is-checked class on buttons
+		$('.button-group .button').on('click', function() {
+			$('.button-group').find('.is-checked').removeClass('is-checked');
+			$(this).addClass('is-checked');
+		});
+
+		//Isotope Load more button
+		var initShow = 22; //number of items loaded on init & onclick load more button
+		var counter = initShow; //counter for load more button
+		var iso = $container.data('isotope'); // get Isotope instance
+
+		loadMore(initShow); //execute function onload
+
+		function loadMore(toShow) {
+			$container.find(".hidden").removeClass("hidden");
+
+			var hiddenElems = iso.filteredItems.slice(toShow, iso.filteredItems.length).map(function(item) {
+				return item.element;
+			});
+			$(hiddenElems).addClass('hidden');
+			$container.isotope('layout');
+
+			//when no more to load, hide show more button
+			if (hiddenElems.length == 0) {
+				jQuery(".more-game").hide();
+			} else {
+				jQuery(".more-game").show();
+			};
+
+		}
+
+		//append load more button
+		$container.after('<a href="#" class="more-game"><span>More games</span></a>');
+
+		//when load more button clicked
+		$(".more-game").click(function() {
+			if ($('.filter-button').data('clicked')) {
+			//when filter button clicked, set initial value for counter
+			counter = initShow;
+			$('.filter-button').data('clicked', false);
+			} else {
+				counter = counter;
+			};
+			counter = counter + initShow;
+			loadMore(counter);
+			return false;
+		});
+
+		//when filter button clicked
+		$(".filter-button").click(function() {
+			$(this).data('clicked', true);
+			loadMore(initShow);
+		});
+
+		//ISOTOPE PLUGIN SCRIPT END//
+
+
+
 	});
 
 }(jQuery));
